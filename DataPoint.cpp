1baad023bfd8445674c019b0ea3b09f9c9d90c4f
RenYang <79951103+RushWithQuattro@users.noreply.github.com>
2022-12-27 10:08:44 -0500

    Add files via upload

---
+++ DataPoint.cpp
@@ -0,0 +1,45 @@
+// #include "DataPoint.h"
+// #include <string>
+
+
+// DataPoint::DataPoint(std::string datetime, double price)
+// {
+// 	this->datetime = datetime;
+// 	this->price = price;
+
+// 	return;
+// }
+
+// // Destructor
+// DataPoint::~DataPoint()
+// {
+// 	return;
+// }
+
+// // Setter of the data point date
+// void DataPoint::set_datetime(std::string datetime)
+// {
+// 	this->datetime = datetime;
+
+// 	return;
+// }
+
+// // Setter of the data point value
+// void DataPoint::set_value(double price)
+// {
+// 	this->price = price;
+
+// 	return;
+// }
+
+// // Getter of the data point date
+// std::string DataPoint::get_datetime()
+// {
+// 	return this->datetime;
+// }
+
+// // Getter of the data point value
+// double DataPoint::get_value()
+// {
+// 	return this->price;
+// }
